5b1f1fa69a36e45266acbc2170dea5e6bc34285a
rautsoham03 <rautsoham02@gmail.com>
2025-12-28 19:58:09 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,14 @@
 import InvestorGuidance from "./components/InvestorGuidance";
+import RegimeTimeline from "./components/RegimeTimeline";
 
 function App() {
-  return <InvestorGuidance />;
+  return (
+    <>
+      <InvestorGuidance />
+      <RegimeTimeline />
+    </>
+  );
 }
 
 export default App;
+
